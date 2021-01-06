bd02ba0bd7ee175c1dbed5baf3a4b0636e0e9087
Dmitriy <uswes123@yandex.ru>
2021-01-06 15:04:40 +0500

    lesson 6 (migration)

---
+++ transport/sanic/routes.py
@@ -0,0 +1,12 @@
+from typing import Tuple
+
+from configs.config import ApplicationConfig
+from context import Context
+from transport.sanic.base import SanicEndpoint
+from transport.sanic.endpoints.health import HealthEndpoint
+
+
+def get_routes(config: ApplicationConfig, context: Context) -> Tuple['SanicEndpoint']:
+    return (
+        HealthEndpoint(config=config, context=context, uri='/', methods=('GET', 'POST')),
+    )
